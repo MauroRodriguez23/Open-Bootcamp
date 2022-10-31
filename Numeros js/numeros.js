2ffd7f9fbb098b6f56bb42e524c89e4a782c20ce
MauroRodriguez23 <102191652+MauroRodriguez23@users.noreply.github.com>
2022-10-31 19:28:27 -0300

    Add files via upload

---
+++ Numeros js/numeros.js
@@ -0,0 +1,16 @@
+let alturaCent = 175;
+let alturaMetr = 1.75;
+let peso = 82.5;
+let alturaMetrRed = 1.80;
+let pesoRed = 80;
+
+let numeroMaxJs = Number.MAX_SAFE_INTEGER;
+let numeroMax1 = numeroMaxJs + 1;
+
+if (numeroMax1 === numeroMaxJs) {
+    console.log("el numero es igual")
+}else {
+    console.log("el numero no es igual")
+}
+
+numeroMax1 = false;
